fb13ea977d5ef94eda036cf696486976a8863730
Prashant Nigam <ajaxbuddy@gmail.com>
2019-10-06 19:34:14 -0400

    Initial Commit

---
+++ SparkleSetupGuide/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SparkleSetupGuide
+//
+//  Created by Prashant Nigam on 10/6/19.
+//  Copyright © 2019 Prashant Nigam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SparkleSetupGuide/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SparkleSetupGuide
+//
+//  Created by Prashant Nigam on 10/6/19.
+//  Copyright © 2019 Prashant Nigam. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
